f7714e316becdf4e6c7a16bf06f8f74decb3459d
Scott Ammon <scott.o.ammon@gmail.com>
2018-04-28 08:40:21 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log('Up and Running!');
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var cardOne = cards[3];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	}
+	else {
+		alert('Sorry, try again.');
+	}
+}
 
 console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardThree);
+console.log('User flipped ' + cardTwo);
